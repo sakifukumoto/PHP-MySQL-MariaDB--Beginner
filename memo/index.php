d927965a9c493894944a057a3e6f0c27074f532e
fukumoto <saki.fukumoto9918@gmail.com>
2023-03-19 08:57:03 +0900

    ページネーションのリンク作成

--- memo/index.php
+++ memo/index.php
@@ -1,6 +1,12 @@
 <?php
 require('dbconnect.php');
 
+//最大のページ数を求める
+$itemCounts = $db->query('select count(*) as cnt from memos');
+$itemCount = $itemCounts->fetch_assoc();
+$max_page = floor(($itemCount['cnt']+1)/5+1);
+echo $max_page;
+
 $stmt = $db->prepare('select * from memos order by id desc limit ?, 5');
 if(!$stmt) {
   die($db->error);
@@ -39,6 +45,15 @@ $result = $stmt->execute();
 <?php if ($count == 0):?>
   <p>表示するメモはありません</p>
 <?php endif; ?>
+
+<p>
+  <?php if($page>1): ?>
+    <a href="?page=<?php echo $page-1; ?>"><?php echo $page-1; ?>ページ目へ</a> |
+  <?php endif; ?>
+  <?php if($page<$max_page): ?>
+    <a href="?page=<?php echo $page+1; ?>"><?php echo $page+1; ?>ページ目へ</a>
+  <?php endif; ?>
+</p>
 
 </body>
 </html>

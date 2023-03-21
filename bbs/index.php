680963fe83698830d62f0fd2720ba644c8a9addd
fukumoto <saki.fukumoto9918@gmail.com>
2023-03-21 19:25:37 +0900

    メッセージの詳細画面の作成

--- bbs/index.php
+++ bbs/index.php
@@ -85,7 +85,7 @@ $name = $_SESSION['name'];
           <img src="./member_picture/?php echo h($picture); ?>" width="48" height="48" alt="" />
         <?php endif; ?>
         <p><?php echo h($message); ?><span class="name"> (<?php echo h($name); ?>)</span></p>
-        <p class="day"><a href="view.php?id="><?php echo h($created); ?></a>
+        <p class="day"><a href="view.php?id=<?php echo h($id); ?>"><?php echo h($created); ?></a>
           [<a href="delete.php?id=" style="color: #F33;">削除</a>]
         </p>
       </div>
